07a5d8bacbc95369d3c7d2de00f7fda2ed1d7aee
Pan Ziyue <hacker@statixind.net>
2014-09-20 12:53:46 +0800

    Initial Commit

---
+++ Places@SST/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Places@SST
+//
+//  Created by Pan Ziyue on 20/9/14.
+//  Copyright (c) 2014 StatiX Industries. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
